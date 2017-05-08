9b3af4b38389ab8545953d60fa50d23d600671b1
Dennis <guoxin.qiu@outlook.com>
2017-05-08 17:05:53 +0800

    add system clock

---
+++ Bonbonniere.Infrastructure/Util/Clock.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bonbonniere.Infrastructure.Util
+{
+    public class Clock : IClock
+    {
+        public DateTime Now => DateTime.Now;
+    }
+}
---
+++ Bonbonniere.Infrastructure/Util/IClock.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Bonbonniere.Infrastructure.Util
+{
+    public interface IClock
+    {
+        DateTime Now { get; }
+    }
+}
